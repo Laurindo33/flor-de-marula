00cd90e12e2d3383f95bdbca713a66692b95af97
Musamidiageral <musamidiageral@gmail.com>
2026-08-25 17:08:25 +0100

    Corrige regressao mobile em "Por que somos Diferentes"
    A troca anterior para layout lado a lado tinha reordenado a marcacao
    (imagem antes do cabecalho), o que alterava tambem a ordem no mobile
    mesmo sem media query dedicada. Volta a ordem original no HTML
    (cabecalho > imagem > lista) e usa CSS Grid apenas no desktop
    (>=992px) para posicionar a imagem a esquerda e cabecalho/lista a
    direita, sem depender da ordem da marcacao. O mobile fica identico
    ao estado anterior a esta seccao de mudancas.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -138,22 +138,20 @@
         <div class="row g-4 justify-content-center">
             <div class="col-12 col-lg-10">
                 <div class="fm-compare-card">
+                    <div class="fm-compare-card__header fm-compare-card__header--us">
+                        <p class="fm-heading-katibeh mb-0">Flor de Marula</p>
+                    </div>
                     <div class="fm-compare-card__media">
                         <img src="{{ asset('images/home/compare-flor-de-marula.jpg') }}" alt="Flor de Marula" class="fm-compare-card__image" loading="lazy">
                     </div>
-                    <div class="fm-compare-card__content">
-                        <div class="fm-compare-card__header fm-compare-card__header--us">
-                            <p class="fm-heading-katibeh mb-0">Flor de Marula</p>
-                        </div>
-                        <ul class="fm-compare-card__list">
-                            @foreach (['Feito Por Nós', 'Ingredientes Naturais', 'Seguro durante a gravidez', 'Livre de químicos, parabenos', 'Pequeno Lote Fresco', 'Feito para Pele de Melanina', '3-5 minutos por dia'] as $item)
-                                <li>
-                                    <img src="{{ asset('images/home/icon-check.png') }}" alt="">
-                                    <span>{{ $item }}</span>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
+                    <ul class="fm-compare-card__list">
+                        @foreach (['Feito Por Nós', 'Ingredientes Naturais', 'Seguro durante a gravidez', 'Livre de químicos, parabenos', 'Pequeno Lote Fresco', 'Feito para Pele de Melanina', '3-5 minutos por dia'] as $item)
+                            <li>
+                                <img src="{{ asset('images/home/icon-check.png') }}" alt="">
+                                <span>{{ $item }}</span>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
